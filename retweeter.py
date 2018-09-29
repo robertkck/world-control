e6f636f624d135595a9a933a64a4ee29268e57f4
robertkck <robert_kck@hotmail.com>
2018-09-30 01:36:05 +0200

    fixed retweet bot credentials missing

--- retweeter.py
+++ retweeter.py
@@ -8,6 +8,7 @@ Created on Fri Sep 28 19:05:49 2018
 import tweepy
 from key import *
 import pickle
+from time import sleep
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -15,4 +16,35 @@ api = tweepy.API(auth)
 if (not api):
     print ("Can't Authenticate")
     sys.exit(-1)
-    
+
+worldcontrol = api.search(q = "@truWorldControl", tweet_mode='extended')
+since_id = worldcontrol[1].id
+                       
+import time
+while True:
+    print("loop")
+    try:
+        new_tweets = api.search(q = "@truWorldControl", since_id = since_id, tweet_mode='extended')
+        if not new_tweets:
+            print("No new tweet")
+        else:
+            for tweet in tweepy.Cursor(api.search, q='@truWorldControl', since_id = since_id, tweet_mode='extended').items(5):
+                print("new tweet found: " + tweet.full_text)
+                try:
+                    if "#fakenewz" not in tweet.full_text:
+                        print("Tweet does not include the #fakenewz Hashtag")
+                    else:
+                        print('\nBot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to respond.')
+                        tweet.retweet()
+                        print('Retweet published successfully.')
+                except tweepy.TweepError as error:
+                    print('\nError. Retweet not successful. Reason: ')
+                    print(error.reason)
+                except StopIteration:
+                    break
+            since_id = tweet.id
+    except tweepy.TweepError as error:
+            print('\nError. Retweet not successful. Reason: ')
+            print(error.reason)
+    print("You keep me waiting waiting waiting for an answer")
+    time.sleep(30)
--- tweetbot.py
+++ tweetbot.py
@@ -37,6 +37,30 @@ from key import *
 from module import *
 import os
 cwd = os.getcwd()
+import logging
+import logging.handlers
+import smtplib 
+#smtpObj = smtplib.SMTP('http://smtp.gmail.com', 587) 
+#smtpObj.starttls()
+#smtpObj.login('pronks@example.com', 'MY_PASSWORD')
+#smtpObj.sendmail('pronKs@example.com', 'quora@example.com', 'Subject: So long.\nDear kruelT, Go to sleep bitch. Sincerely, pronKs')
+#smtpObj.quit()
+#    
+#smtp_handler = logging.handlers.SMTPHandler(mailhost=("smtp.gmail.com", 25),
+#                                            fromaddr="ministervlatin@gmail.com", 
+#                                            toaddrs="robert.kalcik@gmail.com",
+#                                            subject=u"Master, I have failed you!")
+#
+#
+#logger = logging.getLogger()
+#logger.addHandler(smtp_handler)
+#
+#try:
+#    a+1
+#except Exception as e:
+#    logger.exception('Unhandled Exception')
+
+
 from reportlab.lib.pagesizes import A4, landscape
 width, height = landscape(A4)
 cm = 28.346456692913385
@@ -97,78 +121,83 @@ since_id = twitter_history[0]['id']
 import time
 while True:
     print("loop")
-    new_tweets = api.search(q = "@truWorldControl", since_id = since_id, tweet_mode='extended')
-    if not new_tweets:
-        print("No new tweet")
-    else:
-        for tweet in tweepy.Cursor(api.search, q='@truWorldControl', since_id = since_id, tweet_mode='extended').items(5):
-            print("new tweet found: " + tweet.full_text)
-            twitter_history.insert(0, tweet._json)
-            with open('twitter_history', 'wb') as fp:
-                    pickle.dump(twitter_history, fp)
-            df = df.append(twitter_history[0], ignore_index = True)
-            # df.to_csv('master.csv', encoding = 'utf-8')
-            df.to_excel('master.xlsx')
-            
-            try:
-                if "#fakenewz" not in tweet.full_text:
-                    print("Tweet does not include the #fakenewz Hashtag")
-                else:
-                    print('\nBot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to respond.')
-                    # t = process_text(tweet)
-                    for i in range(0,20):
-                        while True:
-                            try: 
-                                t = text2paragraph(tweet.full_text)
-                            except:
-                                continue
-                            break
-                    
-                    _l = _l + [t]
-                    #sheet = labels.Sheet(specs, draw_label, border=True)
-                    #sheet.add_labels(_l[0:25])
-                    #sheet.save('front.pdf')
-                    rev_l = list(reversed(_l))
-                    cards = [rev_l[0:5],rev_l[5:10],rev_l[10:15],rev_l[15:20],rev_l[20:25]]
-                    #Create table
-                    table = Table(cards, 5.9 * cm, 3.88 * cm)
-                    table.setStyle(TableStyle([
-                                           ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
-                                           ('BOX', (0,0), (-1,-1), 0.25, colors.black),
-                                           ('BOTTOMPADDING', (0,0), (-1,-1), 10),
-                                           ('VALIGN',(0,0),(-1,-1),'BOTTOM')
-                                           # ('SIZE', (0,0), (-1,-1), 20)
-                                           ]))
-                    canv = canvas.Canvas('front.pdf', pagesize = landscape(A4))
-                    table.wrapOn(canv, width, height)
-                    table.drawOn(canv, *coord(0.1, 20.2, cm))
-                    canv.save()
-                    # Merge front with back
-                    merger = PdfFileMerger()
-                    merger.append('front.pdf')
-                    merger.append('wc_news_A4_back.pdf')
-                    merger.write('wc_newz.pdf')
-                    # upload_pdf(["wc_newz.pdf", "twitter_history", "master.xlsx"], cwd)
-                    upload_ftp("wc_newz.pdf", ftp_user, ftp_password)
-                    # tweet.retweet("Hello! Find your card ready for print here: https://github.com/robertkck/world-control/raw/master/world-control.pdf")
-                    # m = "@%s Hello! Find your card ready for print here: https://github.com/robertkck/world-control/raw/master/world-control.pdf" % (tweet.user.screen_name)
-                    # Google Drive: https://docs.google.com/gview?url=https://github.com/robertkck/world-control/raw/master/world-control.pdf
-                    # Bitly: https://bit.ly/2PtbTN0
-                    # Shortened Google Drive: https://goo.gl/8xBSZn
-                    # 000: https://bit.ly/2xgnGaJ
-                    # 000: https://goo.gl/fdVavF
-                    m = "@%s BREAKING 🗞️: Find your #fakenewz ready for mass production here: world-control.net/pages/latest-newz" % (tweet.user.screen_name)
-                    print(m)
-                    api.update_status(m, tweet.id)
-                    # with open('outfile', 'wb') as fp:
-                    #         pickle.dump(_l, fp)
-                    time.sleep(10)
-            except tweepy.TweepError as error:
-                print('\nError. Retweet not successful. Reason: ')
-                print(error.reason)
+    try:
+        new_tweets = api.search(q = "@truWorldControl", since_id = since_id, tweet_mode='extended')
+        if not new_tweets:
+            print("No new tweet")
+        else:
+            for tweet in tweepy.Cursor(api.search, q='@truWorldControl', since_id = since_id, tweet_mode='extended').items(5):
+                print("new tweet found: " + tweet.full_text)
+                twitter_history.insert(0, tweet._json)
+                with open('twitter_history', 'wb') as fp:
+                        pickle.dump(twitter_history, fp)
+                df = df.append(twitter_history[0], ignore_index = True)
+                # df.to_csv('master.csv', encoding = 'utf-8')
+                df.to_excel('master.xlsx')
                 
-            except StopIteration:
-                break
-        since_id = twitter_history[0]['id']
+                try:
+                    if "#fakenewz" not in tweet.full_text:
+                        print("Tweet does not include the #fakenewz Hashtag")
+                    else:
+                        print('\nBot found tweet by @' + tweet.user.screen_name + '. ' + 'Attempting to respond.')
+                        # t = process_text(tweet)
+                        for i in range(0,20):
+                            while True:
+                                try: 
+                                    t = text2paragraph(tweet.full_text)
+                                except:
+                                    continue
+                                break
+                        
+                        _l = _l + [t]
+                        #sheet = labels.Sheet(specs, draw_label, border=True)
+                        #sheet.add_labels(_l[0:25])
+                        #sheet.save('front.pdf')
+                        rev_l = list(reversed(_l))
+                        cards = [rev_l[0:5],rev_l[5:10],rev_l[10:15],rev_l[15:20],rev_l[20:25]]
+                        #Create table
+                        table = Table(cards, 5.9 * cm, 3.88 * cm)
+                        table.setStyle(TableStyle([
+                                               ('INNERGRID', (0,0), (-1,-1), 0.25, colors.black),
+                                               ('BOX', (0,0), (-1,-1), 0.25, colors.black),
+                                               ('BOTTOMPADDING', (0,0), (-1,-1), 10),
+                                               ('VALIGN',(0,0),(-1,-1),'BOTTOM')
+                                               # ('SIZE', (0,0), (-1,-1), 20)
+                                               ]))
+                        canv = canvas.Canvas('front.pdf', pagesize = landscape(A4))
+                        table.wrapOn(canv, width, height)
+                        table.drawOn(canv, *coord(0.1, 20.2, cm))
+                        canv.save()
+                        # Merge front with back
+                        merger = PdfFileMerger()
+                        merger.append('front.pdf')
+                        merger.append('wc_news_A4_back.pdf')
+                        merger.write('wc_newz.pdf')
+                        # upload_pdf(["wc_newz.pdf", "twitter_history", "master.xlsx"], cwd)
+                        upload_ftp("wc_newz.pdf", ftp_user, ftp_password)
+                        # tweet.retweet("Hello! Find your card ready for print here: https://github.com/robertkck/world-control/raw/master/world-control.pdf")
+                        # m = "@%s Hello! Find your card ready for print here: https://github.com/robertkck/world-control/raw/master/world-control.pdf" % (tweet.user.screen_name)
+                        # Google Drive: https://docs.google.com/gview?url=https://github.com/robertkck/world-control/raw/master/world-control.pdf
+                        # Bitly: https://bit.ly/2PtbTN0
+                        # Shortened Google Drive: https://goo.gl/8xBSZn
+                        # 000: https://bit.ly/2xgnGaJ
+                        # 000: https://goo.gl/fdVavF
+                        m = "@%s BREAKING 🗞️: Find your #fakenewz ready for mass production here: world-control.net/pages/latest-newz" % (tweet.user.screen_name)
+                        print(m)
+                        api.update_status(m, tweet.id)
+                        # with open('outfile', 'wb') as fp:
+                        #         pickle.dump(_l, fp)
+                        time.sleep(10)
+                except tweepy.TweepError as error:
+                    print('\nError. Retweet not successful. Reason: ')
+                    print(error.reason)
+                    
+                except StopIteration:
+                    break
+            since_id = twitter_history[0]['id']
+    except tweepy.TweepError as error:
+            print('\nError. Retweet not successful. Reason: ')
+            print(error.reason)
+            
     print("You keep me waiting waiting waiting for an answer")
     time.sleep(30)
